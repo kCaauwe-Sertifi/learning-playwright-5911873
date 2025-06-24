b1486ce7e86f9d417c49b16cc8daa4df6dae88d6
Karen Caauwe <105750304+kCaauwe-Sertifi@users.noreply.github.com>
2025-06-24 11:17:47 -0500

    get happy

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://practicesoftwaretesting.com",
     testIdAttribute: "data-test",
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
     actionTimeout: 0,
---
+++ tests/home.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "@playwright/test";
+
+test("Home page", async ({ page })) => {
+  await page.goto("https:practicesoftwaretesting.com/");
+});
